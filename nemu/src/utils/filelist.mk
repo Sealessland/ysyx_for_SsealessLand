e783f4dd4a9c74261d8fe55ca3eddce4bc75312e
240321 <2023010905022@std.uestc.edu.cn>
2025-03-12 22:06:57 +0800

    OSCPU/chisel-playground master initialized
    OSCPU/chisel-playground 08af0f9dd3307ae7d51941bf5cce0671bf19f9c0 Merge pull request #12 from zlh20040308/fix-name

--- nemu/src/utils/filelist.mk
+++ nemu/src/utils/filelist.mk
@@ -16,7 +16,7 @@
 ifeq ($(CONFIG_ITRACE)$(CONFIG_IQUEUE),)
 SRCS-BLACKLIST-y += src/utils/disasm.c
 else
-LIBCAPSTONE = tools/capstone/repo/libcapstone.so.5
+LIBCAPSTONE = tools/capstone/repo/libcapstone.so.6
 CFLAGS += -I tools/capstone/repo/include
 src/utils/disasm.c: $(LIBCAPSTONE)
 $(LIBCAPSTONE):
